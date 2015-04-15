15312e2b581bd311beb519525d82de47647f3fb7
precata <sdesmeduza@gmail.com>
2015-04-15 20:14:03 +0300

    add function language

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,23 +1,54 @@
 <?php
 
-class HomeController extends BaseController {
+class HomeController extends BaseController
+{
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Default Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | You may wish to use controllers instead of, or in addition to, Closure
+    | based routes. That's great! Here is an example controller method to
+    | get you started. To route to this controller, just add the route:
+    |
+    |	Route::get('/', 'HomeController@showWelcome');
+    |
+    */
 
-	public function showWelcome()
-	{
-		return View::make('hello');
-	}
+    public function showWelcome()
+    {
+        return View::make('hello');
+    }
 
+    /**
+     * Set language
+     *
+     * @param string $locale
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function language($locale)
+    {
+        if (!is_null($locale)) {
+            switch ($locale) {
+                case 'en':
+                    $language = 'en';
+                    break;
+                case 'ru':
+                    $language = 'ru';
+                    break;
+                default:
+                    $language = 'ru';
+                    break;
+            }
+
+            if (!empty($language)) {
+                Session::set('_locale', $language);
+            }
+        }
+
+        App::setLocale($language);
+
+        return Redirect::action('IndexController@getIndex');
+    }
 }
