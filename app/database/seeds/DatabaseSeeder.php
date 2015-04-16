14f4716dbf0410e5a79bf9f394f279f5cb2b1556
precata <sdesmeduza@gmail.com>
2015-04-16 21:32:01 +0300

    GenerateRandomUsers&&Planets

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,11 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+        $this->call('UserTableSeeder');
+        $this->command->info('User table seeded!');
+
+        $this->call('PlanetTableSeeder');
+        $this->command->info('Planet table seeded!');
 	}
 
 }
